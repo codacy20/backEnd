186c9c1425e881fd68d6078dcb8a4fdbaf78ead3
ivegotasthma <ivegotasthma@openmailbox.org>
2016-05-12 16:28:57 +0200

    fix: reformat source code

--- app/frontend/js/nav.js
+++ app/frontend/js/nav.js
@@ -1,28 +1,14 @@
 $(document).ready(function () {
-//   $(".icon").click(function () {
-//     $(".mobilenav").fadeToggle(500);
-//     $(".top-menu").toggleClass("top-animate");
-//     $(".mid-menu").toggleClass("mid-animate");
-//     $(".bottom-menu").toggleClass("bottom-animate");
-//   });
-//
-//   $('body').click(function(e) {
-//     if (!$(e).target().is('.icon'))
-//         $('.mobilenav').hide();
-// });
-
-var show = true;
-$('.icon').click(function(){
-  if (show == true) {
-    $(".mobilenav").fadeToggle(500);
-    $(".top-menu").toggleClass("top-animate");
-    $(".mid-menu").toggleClass("mid-animate");
-    $(".bottom-menu").toggleClass("bottom-animate");
-  }
-  else if (show == false) {
-    $(".mobilenav").hide();
-  }
-  });
-
-  
+    var show = true;
+    $('.icon').click(function () {
+        if (show == true) {
+            $(".mobilenav").fadeToggle(500);
+            $(".top-menu").toggleClass("top-animate");
+            $(".mid-menu").toggleClass("mid-animate");
+            $(".bottom-menu").toggleClass("bottom-animate");
+        }
+        else if (show == false) {
+            $(".mobilenav").hide();
+        }
+    });
 });
