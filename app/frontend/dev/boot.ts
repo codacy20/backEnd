c2f92cbb5a3ceed6d63ac013cd72c06ef0304865
adjoawadee <ernestina.wad@gmail.com>
2016-05-28 10:05:24 -0700

    sign in
    sign in in progress

--- app/frontend/dev/boot.ts
+++ app/frontend/dev/boot.ts
@@ -1,5 +1,5 @@
 ///<reference path="../typings/browser.d.ts"/>
 import {bootstrap} from '@angular/platform-browser-dynamic';
-import {AppComponent} from "./app.component";
+import {AppComponent} from "./app/app.component.ts";
 
 bootstrap(AppComponent);
