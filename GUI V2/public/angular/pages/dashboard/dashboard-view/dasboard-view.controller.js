d8c72d385b226681f249d1559fe0a7083950e608
adjoawadee <ernestina.wad@gmail.com>
2016-06-28 23:24:36 +0200

    in progress.... restaurant preview page

--- GUI V2/public/angular/pages/dashboard/dashboard-view/dasboard-view.controller.js
+++ GUI V2/public/angular/pages/dashboard/dashboard-view/dasboard-view.controller.js
@@ -1,18 +1,42 @@
 (function () {
 
-function DashboardViewCtrl ($http, $state) {
+function DashboardViewCtrl ($http, $state, $uibModal) {
     var vm = this;
 
-    function init() {
 
+    vm.showModal = function(){
+        $uibModal.open({
+            templateUrl: 'addModalContent.html', //change this
+            controller: "AddMenuModalController", //change this
+            controllerAs: 'modal' //change this
+        })
+            .result.then(
+            function () {
+                //ok
+            },
+            function () {
+                //cancelled
+            }
+        );
     }
 
-
 }
+    function AddMenuModalController ($uibModalInstance) {
+        var vm = this;
+
+        vm.ok = function () {
+            $uibModalInstance.close();//change the param
+        };
+
+        vm.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+    }
 
 
 angular
     .module('app')
-    .controller('DashboardViewCtrl', DashboardViewCtrl);
+    .controller('DashboardViewCtrl', DashboardViewCtrl)
+    .controller('AddMenuModalController', AddMenuModalController);
 }());
 
